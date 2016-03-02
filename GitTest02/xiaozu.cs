b9587c28f64361f525bc5595ed4e42b350ac46d7
1102137211 <1102137211@gm.kuas.edu.tw>
2016-03-03 07:29:03 +0800

    sl;m

---
+++ GitTest02/xiaozu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GitTest02
+{
+    public class xiaozu
+    {
+        public static void main()
+        {
+            System.Console.WriteLine("bigfatZU");
+            System.Console.ReadLine();
+        }
+    }
+}
